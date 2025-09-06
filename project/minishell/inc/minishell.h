40a495834941e58f613694ab58207f64e076cd28
p0yo-lang <p0y.on@protonmail.com>
2025-09-06 17:38:51 +0200

    feat(parsing): add t_command structure and rename get_command_list to get_token_list

--- project/minishell/inc/minishell.h
+++ project/minishell/inc/minishell.h
@@ -6,49 +6,24 @@
 /*   By: mmacedo- <mmacedo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 18:24:47 by mmacedo-          #+#    #+#             */
-/*   Updated: 2025/08/31 23:39:40 by mmacedo-         ###   ########.fr       */
+/*   Updated: 2025/09/06 17:46:38 by mmacedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <stdio.h>
-# include <readline/readline.h>
-# include <curses.h>
-# include <term.h>
+# include "parsing.h"
 
-typedef enum e_quote_flag
-{
-	NOT_IN_QUOTES,
-	IN_SINGLE_QUOTES,
-	IN_DOUBLE_QUOTES
-}	t_quote_flag;
-
-typedef enum e_token_type
-{
-	OPERATOR,
-	WORD
-}	t_token_type;
-
-typedef struct s_token
-{
-	char			*content;
-	t_token_type	token_type;
-	struct s_token	*next;
-}	t_token;
-
-char			*buffer_add(char *buffer, char c);
-char			**get_command(char *command);
-void			flush_buffer_to_token(t_token **current_token, char **buffer);
-void			free_token_list(t_token *token_head);
-void			populate_buffer(char c, t_quote_flag *quote_flag,
-					char **buffer);
-int				is_content_empty(char *buffer);
-int				is_reddirection(char *buffer);
-int				create_redirection_token(char *command, int i, t_token **current_token);
-t_token			*create_token(char *content, t_token_type token_type);
-t_token			*get_command_list(char *command);
-t_token_type	get_token_type(char	*buffer);
+/*
+** get_token_list:
+** Convert a string into a chained link of tokens.
+** Parameters:
+**  - command : input string provied by the user
+** Return:
+**	- head of the token list
+*/
+t_token			*get_token_list(char *command);
+char			*get_path(char *command, char **envp);
 
 #endif
